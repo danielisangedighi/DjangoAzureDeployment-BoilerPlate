9b02501b37486c2c5297489b627d68a1d9ea3e91
Daniel Isangedighi <danielisangedighi@outlook.com>
2025-01-04 18:47:16 +0100

    final commit

--- project/settings.py
+++ project/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
+
+if 'WEBSITE_HOSTNAME' in os.environ: # Running on Azure
+    from .azure import *
 
 from pathlib import Path
 
